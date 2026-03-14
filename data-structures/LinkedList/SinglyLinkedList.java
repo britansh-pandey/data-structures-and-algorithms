aa2351732922b3592e6d2d37a946068fbd4ccc1a
Code-With-Britansh <skp272534@gmail.com>
2026-03-15 04:59:03 +0530

    Update SinglyLinkedList.java

--- data-structures/LinkedList/SinglyLinkedList.java
+++ data-structures/LinkedList/SinglyLinkedList.java
@@ -1,1 +1,80 @@
+public class SinglyLinkedList {
 
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    void insert(int data) {
+
+        Node newNode = new Node(data);
+
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    void delete(int key) {
+
+        if(head == null) return;
+
+        if(head.data == key) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp.next != null && temp.next.data != key) {
+            temp = temp.next;
+        }
+
+        if(temp.next != null) {
+            temp.next = temp.next.next;
+        }
+    }
+
+    void display() {
+
+        Node temp = head;
+
+        while(temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+
+        list.display();
+
+        list.delete(20);
+
+        list.display();
+    }
+}
