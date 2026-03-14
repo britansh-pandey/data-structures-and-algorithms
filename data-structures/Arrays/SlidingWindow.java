fccf05ef3223d5438119eada954574aeb8ce73fc
Code-With-Britansh <skp272534@gmail.com>
2026-03-15 04:51:54 +0530

    Update SlidingWindow.java

--- data-structures/Arrays/SlidingWindow.java
+++ data-structures/Arrays/SlidingWindow.java
@@ -1,1 +1,34 @@
+public class SlidingWindow {
 
+    static int maxSumSubarray(int arr[], int k) {
+
+        int windowSum = 0;
+
+        for(int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        for(int i = k; i < arr.length; i++) {
+
+            windowSum = windowSum - arr[i - k] + arr[i];
+
+            if(windowSum > maxSum) {
+                maxSum = windowSum;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+
+        int result = maxSumSubarray(arr, k);
+
+        System.out.println(result);
+    }
+}
