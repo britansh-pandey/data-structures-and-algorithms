239e74ee18aa634b872ee27305e001dc306a5a04
Code-With-Britansh <skp272534@gmail.com>
2026-03-15 04:56:05 +0530

    Implement two-pointer technique to find pair sum

--- data-structures/Arrays/TwoPointerTechnique.java
+++ data-structures/Arrays/TwoPointerTechnique.java
@@ -1,1 +1,38 @@
+public class TwoPointer {
 
+    static boolean findPair(int arr[], int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if(sum == target) {
+                System.out.println(arr[left] + " " + arr[right]);
+                return true;
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 3, 4, 6, 8, 9};
+        int target = 10;
+
+        boolean result = findPair(arr, target);
+
+        if(!result) {
+            System.out.println("No pair found");
+        }
+    }
+}
