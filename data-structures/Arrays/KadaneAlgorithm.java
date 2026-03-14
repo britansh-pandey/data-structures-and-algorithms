96d05ef31d71db3975a1368a2352369649ab9952
Code-With-Britansh <skp272534@gmail.com>
2026-03-15 04:44:15 +0530

    Implement Kadane's Algorithm for maximum subarray sum

--- data-structures/Arrays/KadaneAlgorithm.java
+++ data-structures/Arrays/KadaneAlgorithm.java
@@ -1,1 +1,33 @@
+public class KadaneAlgorithm {
 
+    static int maxSubArray(int arr[]) {
+
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+
+            if(currentSum + arr[i] > arr[i]) {
+                currentSum = currentSum + arr[i];
+            } 
+            else {
+                currentSum = arr[i];
+            }
+
+            if(currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+
+        int result = maxSubArray(arr);
+
+        System.out.println(result);
+    }
+}
