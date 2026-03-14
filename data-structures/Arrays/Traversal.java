929a9cb0d916e89a5e282d7713d8613d4923af9e
Code-With-Britansh <skp272534@gmail.com>
2026-03-15 04:53:17 +0530

    Implement array traversal in Traversal class

--- data-structures/Arrays/Traversal.java
+++ data-structures/Arrays/Traversal.java
@@ -1,1 +1,16 @@
+public class Traversal {
 
+    static void traverse(int arr[]) {
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {10, 20, 30, 40, 50};
+
+        traverse(arr);
+    }
+}
